bd3a49102e409db7380656329ddf796c6b9a7c24
MarcoGRodriguezR <rodriguezmg05@gmail.com>
2024-11-20 17:08:28 -0700

    2

--- MenuPrincipal.py
+++ MenuPrincipal.py
@@ -1,7 +1,20 @@
+
+
 import tkinter as tk
 
-#Valores
-inventario = {}
+
+# Funciones
+def prueba():
+    Boton = tk.Button(menuPrincipal, text= "?", width= 25, height= 3)
+    Boton.grid(row= 2, column= 25)
+
+def main():
+    while True:
+        for i in range(len(inventario)):
+            print(i)
+
+
+
 
 # Tkinter
 menuPrincipal = tk.Tk()
@@ -10,7 +23,10 @@ menuPrincipal.title("Control de Inventario")
 Titulo = tk.Label(menuPrincipal, text= "Control de Inventario", width= 25)
 Titulo.grid(row= 1, column= 1)
 
-BotonAgregar = tk.Button(menuPrincipal, text= "Agregar Producto", width= 15)
+BotonAgregar = tk.Button(menuPrincipal, text= "Agregar Producto", width= 15, command= prueba)
 BotonAgregar.grid(row= 2, column= 1)
 
 menuPrincipal.mainloop()
+
+if __name__ == "__main__":
+    main()
